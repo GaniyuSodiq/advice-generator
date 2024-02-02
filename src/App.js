2ef4a22f6603c60f8ee9e3f0b6fcb77dd1d0196b
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-02-02 16:14:19 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-async function getAdvice() {
-  const res = await fetch("https://api.adviceslip.com/advice");
-  const data = await res.json();
-  console.log(data);
-
-  return data;
-}
+import { useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data);
+
+    return setAdvice(data.slip.advice);
+  }
+
   return (
     <div className="App">
       <button onClick={getAdvice}>GET ADVICE</button>
+      <p>{advice}</p>
     </div>
   );
 }
